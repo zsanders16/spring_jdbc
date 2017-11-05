69784245bad90cd9960161f47e9aeea60f69dadf
Zachary Sanders <zsanders@icloud.com>
2017-11-05 16:40:15 -0700

    switch the datasource to be created with annotations instead of xml

--- src/main/java/com/sanders/springdemo/daoimpl/OrganizationDaoImpl.java
+++ src/main/java/com/sanders/springdemo/daoimpl/OrganizationDaoImpl.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.sanders.springdemo.dao.OrganizationDao;
 import com.sanders.springdemo.domain.Organization;
 
+@Repository("orgDao")
 public class OrganizationDaoImpl implements OrganizationDao {
 	
 	private JdbcTemplate jdbcTemplate;
 
+	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 
